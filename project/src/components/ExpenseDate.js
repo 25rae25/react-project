5daea76d2f69724a7b57d8d05ec7c1c57a9f818f
25rae25 <whdudfo0124@gmail.com>
2023-02-01 15:18:43 +0900

    컴포지션 컴포넌트, Card.js

---
+++ project/src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import './ExpenseDate.css';
+
+const ExpenseItem = (props) => {
+	const month =  props.date.toLocaleString('ko', {month: 'long'});
+	const day =  props.date.toLocaleString('ko', {day: '2-digit'});
+	const year =  props.date.getFullYear();
+
+	return (
+		<div className="expense-date">
+			<div className="expense-date__month">{month}</div>
+			<div className="expense-date__year">{day}</div>
+			<div className="expense-date__day">{year}</div>
+		</div>
+	);
+}
+
+export default ExpenseItem;
--- project/src/components/ExpenseItem.js
+++ project/src/components/ExpenseItem.js
@@ -1,19 +1,36 @@
- import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
+import './ExpenseItem.css';
  
- function ExpenseItem() {
-	const expenseDate = new Date(2023, 2, 1);
-	const expenseTitle = 'Car Insurance';
-	const expenseAmount = 294.67; 
+// JSX 동적데이터 출력방식 
+//  function ExpenseItem() { 
+// 	const expenseDate = new Date(2023, 2, 1);
+// 	const expenseTitle = 'Car Insurance';
+// 	const expenseAmount = 294.67; 
 
-	return (
-		<div className='expense-item'>
-			<div>{expenseDate.toISOString()}</div>
-			<div className='expense-item__description'>
-				<h2>{expenseTitle}</h2>
-				<div className='expense-item__price'>${expenseAmount}</div>
-			</div>
-		</div>
+// 	return (
+// 		<div className='expense-item'>
+// 			<div>{expenseDate.toISOString()}</div>
+// 			<div className='expense-item__description'>
+// 				<h2>{expenseTitle}</h2>
+// 				<div className='expense-item__price'>${expenseAmount}</div>
+// 			</div>
+// 		</div>
 		
+// 	);
+//  }
+
+
+// props component
+function ExpenseItem(props) { 
+	return (
+		<Card className='expense-item'>
+			<ExpenseDate date={props.date} />
+			<div className='expense-item__description'>
+				<h2>{props.title }</h2>
+				<div className='expense-item__price'>${props.amount}</div>
+			</div>
+		</Card>
 	);
  }
 
