d225ca55233dcbb48acd8f38601445e478110ca1
25rae25 <whdudfo0124@gmail.com>
2023-02-01 21:10:05 +0900

    listening add

--- project/src/components/Expenses/ExpenseItem.js
+++ project/src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,6 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
  
 // JSX 동적데이터 출력방식 
@@ -22,14 +23,23 @@ import './ExpenseItem.css';
 
 
 // props component
-function ExpenseItem(props) { 
+const ExpenseItem = (props) => { 
+
+	const [title, setTitle] = useState(props.title);
+
+	const clickHandler = () => {
+		setTitle('Updated');
+		console.log(title)
+	}
+
 	return (
 		<Card className='expense-item'>
 			<ExpenseDate date={props.date} />
 			<div className='expense-item__description'>
-				<h2>{props.title }</h2>
+				<h2>{title}</h2>
 				<div className='expense-item__price'>${props.amount}</div>
 			</div>
+			<button onClick={clickHandler}>Change Title</button>
 		</Card>
 	);
  }
