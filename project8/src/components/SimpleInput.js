e63cc4b5ae94f61378c5e6978de693420ecd2372
25rae25 <whdudfo0124@gmail.com>
2023-02-08 15:22:25 +0900

    함수 따로 빼서 훅으로 사용하는법

--- project8/src/components/SimpleInput.js
+++ project8/src/components/SimpleInput.js
@@ -1,65 +1,58 @@
 import { useState } from 'react';
+import useInput from './hooks/use-input';
 
 const SimpleInput = (props) => {
- 
-  const [enteredName, setEnteredName] = useState('');
-  // const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
-  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-  // const [formIsValid, setFormIsValid] = useState(false);
-  
-  // useEffect(() => {
-  //   if (enteredNameIsValid) {
-  //     console.log('Name Input is valid!');
-  //   }
-  // }, [enteredNameIsValid]);
+  const { 
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError, 
+    valueChangeHandler: nameChangedHandler, 
+    inputBlurHandler: nameBlurHandler, 
+    reset: restNameInput
+  } = useInput(value => value.trim() !== '');
 
-  const enteredNameIsValid = enteredName.trim() !== '';
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+
+  const enteredEmailIsValid = enteredEmail.includes('@');
+  const enteredEmailIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
 
   let formIsValid = false;
 
-  if(enteredNameIsValid) {
+  if (enteredNameIsValid && enteredEmailIsValid) {
     formIsValid = true;
   }
 
-  const nameInputChangeHandler = (event) => {
-    setEnteredName(event.target.value);
-
-    // if (event.target.value.trim() !== '') {
-    //   setEnteredNameIsValid(true);
-    // }
+  const emailInputChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
   };
 
-  const nameInputBlurHandler = (evnet) => {
-    setEnteredNameTouched(true);
 
-    // if (enteredName.trim() === '') {
-    //   setEnteredNameIsValid(false);
-    // }
+  const emailInputBlurHandler = (event) => {
+    setEnteredEmailTouched(true);
   };
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
-    setEnteredNameTouched(true);
-
-    if (enteredNameIsValid) {
+    if (!enteredNameIsValid) {
       return;
     }
 
     console.log(enteredName);
 
-    // const enteredValue = nameInputRef.current.value;
-    // console.log(enteredValue);
+    // nameInputRef.current.value = ''; => ref 방법
+    restNameInput();
 
-    // nameInputRef.current.value = ''; => ref 방법인데 좋은 방법은 아니다
-    setEnteredName('');
-    setEnteredNameTouched(false);
+    setEnteredEmail('');
+    setEnteredEmailTouched(false);
   };
 
-  
+  const nameInputClasses = nameInputHasError
+    ? 'form-control invalid'
+    : 'form-control';
 
-  const nameInputClasses = nameInputIsInvalid
+  const emailInputClasses = enteredEmailIsInvalid
     ? 'form-control invalid'
     : 'form-control';
 
@@ -68,15 +61,27 @@ const SimpleInput = (props) => {
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input
-          // ref={nameInputRef}
           type='text'
           id='name'
-          onChange={nameInputChangeHandler}
-          onBlur={nameInputBlurHandler}
+          onChange={nameChangedHandler}
+          onBlur={nameBlurHandler}
           value={enteredName}
         />
-        {nameInputIsInvalid && (
+        {nameInputHasError && (
           <p className='error-text'>Name must not be empty.</p>
+        )}
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor='email'>Your E-Mail</label>
+        <input
+          type='email'
+          id='email'
+          onChange={emailInputChangeHandler}
+          onBlur={emailInputBlurHandler}
+          value={enteredEmail}
+        />
+        {enteredEmailIsInvalid && (
+          <p className='error-text'>Please enter a valid email.</p>
         )}
       </div>
       <div className='form-actions'>
